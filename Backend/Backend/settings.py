e96dcaecbe5c7f311e2939179601e75a39888322
DevaKrishna07 <devakrishna1418@gmail.com>
2025-06-19 11:02:44 +0530

    changes to env added gitignore

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov@$omo*5+&+whclm0=x%-k^arzthbuk$3prdg$%)w_z+%**=b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +30,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 STATIC_URL = '/static/'
+
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-default-key')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+
 
 
 # Application definition
